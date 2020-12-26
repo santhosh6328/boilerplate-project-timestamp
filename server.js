cbaf8b5c27f6b8d1cedd32403e5025c3d53d3be3
santhosh <santhosh6328@gmail.com>
2020-12-26 21:24:38 +0530

    added tasks

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { json, request, response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,6 +25,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp", (req, res) => {
+  let date = Date.now()
+  GMTdate = new Date(date);
+  let result = {
+    "unix": date,
+    "utc": GMTdate.toGMTString()
+  }
+  res.send(result);
+});
+
+app.get("/api/timestamp/:date", (req, res) => {
+  if(new Date(req.params.date).getTime() > 0) {
+    var date = new Date(req.params.date.toString());
+  }else{
+    var date = new Date(+req.params.date);
+  }
+  let result = {
+    "unix": date.getTime(),
+    "utc": date.toGMTString()
+  }
+  console.log(result);
+  res.send(result);
+});
 
 
 // listen for requests :)
